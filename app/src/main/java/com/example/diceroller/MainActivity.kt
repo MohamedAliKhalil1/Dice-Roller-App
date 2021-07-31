a7653b3a89773f5c96a26b0500518c4ccf09dc07
unknown <mohamedalikahlil5@gmail.com>
2021-07-31 12:55:02 +0200

    update text view on button press with random number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton : Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
+            val textView : TextView = findViewById(R.id.textView)
+            textView.text = (1..6).random().toString()
             Toast.makeText(this, "Dice rolled !!", Toast.LENGTH_SHORT).show()
         }
     }
